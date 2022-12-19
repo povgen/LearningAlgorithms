04442ad6f9db9565f88249ca595aad12961c5711
Gennadii <povgen@yandex.ru>
2022-12-20 00:56:59 +0300

    invistigate, how work quickSorttrying write second version binarySearchRecursive

--- ConsoleApp1/Algorithms.cs
+++ ConsoleApp1/Algorithms.cs
@@ -24,6 +24,40 @@ public static class Algorithms
         return null;
 	}
 
+    public static int? BinarySearchRecursive(int[] sortedArray, int searchValue) => BinarySearchRecursive(sortedArray, searchValue, 0, sortedArray.Length-1);
+
+
+    public static int? BinarySearchRecursive(int[] sortedArray, int searchValue, int low, int hight)
+	{
+		if(low > hight) return null;
+
+        int mid = (Int32)Math.Round((low + hight) / 2.0);
+
+		if (sortedArray[mid] == searchValue) return mid;
+
+		if (sortedArray[mid] > searchValue) hight = mid - 1;
+        else low = mid + 1;
+
+        return BinarySearchRecursive(sortedArray, searchValue, low, hight);
+
+    }
+
+	public static int? BinarySearchRecursiveSecVer(int[] sortedArray, int searchValue) => BinarySearchRecursiveSecVer(new List<int>(sortedArray), searchValue);
+
+    public static int? BinarySearchRecursiveSecVer(List<int> sortedArray, int searchValue, int offset = 0)
+    {//here somwhe error on searchValue = 3
+        if (sortedArray.Count == 0) return -5;
+        if (sortedArray.Count == 1) return sortedArray[0] == searchValue ? searchValue : null;
+
+        int mid = (Int32)Math.Floor(sortedArray.Count / 2.0);
+
+		if (sortedArray[mid] == searchValue) return offset + mid;
+
+
+		return sortedArray[mid] > searchValue ? BinarySearchRecursiveSecVer(sortedArray.GetRange(0, mid), searchValue, offset) 
+			: BinarySearchRecursiveSecVer(sortedArray.GetRange(mid, sortedArray.Count - 1), offset + mid);
+
+    }
 
     /// <param name="arr"></param>
     /// <returns>index of smallest elemnt of array</returns>
@@ -68,7 +102,7 @@ public static class Algorithms
 
     public static int Count(int[] arr) => Count(new Stack<int>(arr));
 
-    public static int Count(Stack<int> arr)
+    public static int Count(Stack<int> arr)	
 	{
 		if (arr.Count == 0) return 0;
 		arr.Pop();
@@ -86,5 +120,73 @@ public static class Algorithms
 
     }
 
+	public static int MaxVerSec(int[] arr) => MaxVerSec(new List<int>(arr));
+
+    public static int MaxVerSec(List<int> arr)
+    {
+        if (arr.Count == 2) return arr[0] > arr[1] ? arr[0] : arr[1];
+
+		int sub_max = MaxVerSec(arr.GetRange(1, arr.Count - 1));
+
+		return arr[0] > sub_max ? arr[0] : sub_max;
+
+    }
+
+	public static int[] QuickSort(int[] arr)
+	{
+		if (arr.Length < 2) return arr;
+		int[] pivot = { arr[0] };
+
+        Stack<int> less = new Stack<int>();
+        Stack<int> greater = new Stack<int>();
+
+		for (int i = 1; i < arr.Length; i++)
+		{
+			if (arr[i] < pivot[0]) less.Push(arr[i]);
+			else greater.Push(arr[i]);
+		}
+
+
+		return QuickSort(less.ToArray<int>()).Concat(pivot.Concat(QuickSort(greater.ToArray<int>()))).ToArray<int>();
+
+    }
+
+
+
+    public static int[] QuiickSortV2(int[] array) => QuiickSortV2(array, 0, array.Length-1);
+    public static int[] QuiickSortV2(int[] array, int leftIndex, int rightIndex) //из интернета
+    {
+        var i = leftIndex;
+        var j = rightIndex;
+        var pivot = array[leftIndex];
+
+        while (i <= j) // пока не дошли до середины (т.е. пока слева все эл-ты не стали 
+        {
+            while (array[i] < pivot) { i++; } // доходим до элемента, который нужно перенести на другую сторону (вправо от опорного эл-та)
+
+            while (array[j] > pivot) { j--; } // доходим до элемента, который нужно перенести на другую стороне (слева от эл-та)
+
+            if (i <= j) //если индексы не зашли друг за друга (напирмер все эл-ты в массиве одинаковые)
+            {
+                //меняем эл-ты местами
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                //смещаем указатели т.к. поменянные эл-ты  уже отсортированы относительно pivot
+                i++;
+                j--;
+            }
+        }
+
+        if (leftIndex < j)
+            QuiickSortV2(array, leftIndex, j); //иииии, что массив как-то по ссылке передаётся или что, как результат сортировки сохраняется 
+
+        if (i < rightIndex)
+            QuiickSortV2(array, i, rightIndex);
+
+        return array;
+    }
+
+    public static void Test(int[] arr) { for (int i = 0; i < arr.Length; i++) arr[i] = -99; }
 
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using myProject;
 int[] sortedArr = { 0, 2, 3, 5, 6, 7, 8, 9, 10, 12, 22, 45, 77, 78 };
 List<int> unsortedList = new() { 3, 4, 5, 2, 0, 9, 3, 2, 7, 90, 54, 22, 4, 0, 2};
+int[] unsortedArr = { 3, 4, 5, 2, 0, 9, 3, 2, 7, 90, 54, 22, 4, 0, 2};
 
 string[] mods =
 {
@@ -11,18 +12,23 @@ string[] mods =
     "4 - Recursive sum",
     "5 - Recursive count",
     "6 - Recursive max",
+    "7 - Binary search recursive",
+    "8 - Recursive max - book's resolve",
+    "9 - QuickSort"
+
 };
 
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Please select mode:\n" + string.Join('\n', mods));
 int mode = Helper.GetIntFromUser(); // 3
+int searchVal;
 switch (mode)
 {
     case 1:
         Console.WriteLine("Please enter searched value");
 
-        int searchVal = Helper.GetIntFromUser();
+        searchVal = Helper.GetIntFromUser();
 
         int i;
         int? res = Algorithms.BinarySearch(sortedArr, searchVal, out i);
@@ -42,6 +48,28 @@ switch (mode)
     case 4: Console.WriteLine("Sum ("+ string.Join(", ", sortedArr)+") = " + Algorithms.Sum(sortedArr)); break;
     case 5: Console.WriteLine("Count ("+ sortedArr.Count()+ ") ("+ string.Join(", ", sortedArr)+") = " + Algorithms.Count(sortedArr)); break;
     case 6: Console.WriteLine("Max ("+ sortedArr.Max()+ ") ("+ string.Join(", ", sortedArr)+") = " + Algorithms.Max(sortedArr)); break;
+
+    case 7:
+        Console.WriteLine("Please enter searched value");
+
+        searchVal = Helper.GetIntFromUser();
+
+        var searched = Algorithms.BinarySearchRecursive(sortedArr, searchVal);
+        var searched2 = Algorithms.BinarySearchRecursiveSecVer(sortedArr, searchVal);
+
+        if (searched == null) searched = -1;
+        if (searched2 == null) searched2 = -1;
+            Console.WriteLine($"Index searched num: {searched.Value} / {searched2.Value}.");
+     break;
+
+    case 8: Console.WriteLine("Max (" + sortedArr.Max() + ") (" + string.Join(", ", sortedArr) + ") = " + Algorithms.MaxVerSec(sortedArr)); break;
+
+
+    case 9:
+        Console.WriteLine(string.Join(' ', unsortedList));
+        Console.WriteLine(string.Join(' ', Algorithms.QuickSort(unsortedArr)));
+    break;
+
     default:
         Console.WriteLine($"Entered mode not found!");
     break;
